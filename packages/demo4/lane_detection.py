7218a3af81fc77cd27cc68c6f65d868e7eadce26
Hemanth Kumar K <hemanthkumark853@gmail.com>
2020-03-01 20:09:52 +0000

    Add files via upload

--- packages/demo4/lane_detection.py
+++ packages/demo4/lane_detection.py
@@ -20,6 +20,14 @@ clrfilter = cv2.bitwise_and(hsv_image,hsv_image,mask=yellow |white)
 color_lane = cv2.cvtColor(clrfilter,cv2.COLOR_HSV2BGR)
 gray_image = cv2.cvtColor(color_lane,cv2.COLOR_BGR2GRAY)
 gray_gaussian = cv2.GaussianBlur(gray_image,(3,3),0)
-edges = cv2.Canny(gray_gaussian,200,255)
+edges = cv2.Canny(gray_gaussian,75,150)
 cv2.imshow('edges',edges)
+lines = cv2.HoughLinesP(edges,1,np.pi/180,20,maxLineGap=100)
+
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(color_lane, (x1, y1), (x2, y2), (255, 0, 0), 3)
+   
+cv2.imshow('lines',color_lane)
 cv2.waitKey(0)
