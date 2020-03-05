fd02f9d728cd93445a24d2baf7a68d9249d01f23
Ronak Sharma <ronaksharma@dyn217042.shef.ac.uk>
2020-03-05 10:20:10 +0000

    Added Lap Counting Functionality

--- packages/demo4/LapCounting.py
+++ packages/demo4/LapCounting.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-from time
-
+import math
+import time
 #defining the color range
 lower_red=np.array([160,150,0],np.uint8)
 upper_red=np.array([200,255,255],np.uint8)
@@ -9,18 +9,23 @@ red_detected=False
 
 #  variables reresenting area  and position limitation of the colors
 #Red
-contour_w=50
-contour_h=50
-red_area = 600
+red_area = 200
 
-
+perLapInformation=0
 #Identifying the camera and opening it
 cap=cv2.VideoCapture(0)
 ret, frame = cap.read()
 
 height=frame.shape[0]
 width = frame.shape[1]
-number_of_laps=0
+
+time_redNotDetected=time.time()
+time_redDetected=time.time()
+
+lapDetected=0
+number_of_lap=0
+counter=0
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -32,28 +37,29 @@ while(True):
     kernel = np.ones((5,5),np.uint8)
     redmask = cv2.dilate(red,kernel,iterations = 1)
     redmask = cv2.erode(redmask,kernel,iterations =1)
-    
     clrfilter = cv2.bitwise_and(hsv_image,hsv_image,mask=redmask)
     
-    cnts = cv2.findContours(red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        x,y,w,h = cv2.boundingRect(c)
-        if( (w>contour_w or h>contour_h) and y<height/2):
-            red_detected =True;
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (36,255,12), 2)
+    contours, hierarchy = cv2.findContours(redmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+    for plc,contour in enumerate(contours):
+        area=cv2.contourArea(contour)
+        if area>red_area:
+            red_detected=True
+        else:
+           red_detected=False
     cv2.imshow('red',red)# Shows the frame
     
     if red_detected:
-        print('Red is present')
-        t0 = time.time()
-        t1 = time.time()
-        if()
-        number_of_laps=number_of_laps+1
-        sleep(5)
+        counter=counter+1
     else:
-        None
+        counter=0
+    
+    if(counter==50):
+        number_of_lap=number_of_lap+1
         
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+        
+    print(counter,number_of_lap)
     
